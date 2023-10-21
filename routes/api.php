a7b68c1f9fa87f97b2542b0193bf48f458ac501a
kelvincodex <morahthankgod@gmail.com>
2023-10-21 04:34:12 +0100

    error

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    $dir = __DIR__ . "/route/v1/api";
+    $dir = __DIR__ . "/route/api/v1";
 
     foreach (scandir($dir) as $filename) {
 
